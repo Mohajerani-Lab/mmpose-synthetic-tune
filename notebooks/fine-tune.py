5c89d23e1576641ca580443a54498c1cf93539c6
Ali Goldani <ali.goldani97@gmail.com>
2024-04-15 13:30:10 -0400

    training tool added

--- notebooks/fine-tune.py
+++ notebooks/fine-tune.py
@@ -1,21 +1,75 @@
+#%%
+from mmpose.apis import MMPoseInferencer
 from datetime import datetime
-import pytz
 import subprocess
 
-montreal_tz = pytz.timezone('America/Montreal')
-now_utc = datetime.now(pytz.utc)
-now_montreal = now_utc.astimezone(montreal_tz)
-current_time = now_montreal.strftime('%Y-%m-%d_%H-%M-%S')
 
-notes = 'trained-on-ap10k'
+class MMPoseModelCoach:
+    command = 'python'
+    script = 'tools/train.py'
+    detector_model = {  #rtmdet
+        "det_model": 'mmdetection/configs/rtmdet/rtmdet_l_swin_b_p6_4xb16-100e_coco.py',
+        "det_weights": 'checkpoints/rtmdet_l_swin_b_p6_4xb16-100e_coco-a1486b6f.pth',
+    }
 
-args = [
-    'python',
-    'tools/train.py',
-    'mmpose-synthetic-tune/dataset-coco/custom-configs/td-hm_hrnet-w48_8xb64-210e_20kp-256x256.py',
-    '--work-dir',
-    f'mmpose-synthetic-tune/models/train-{current_time}-{notes}',
-    '--resume',
-]
+    def __init__(self, config, resume=True, notes=''):
+        current_time = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
 
-subprocess.run(args)
+        self.config = config
+        self.config_path = f'mmpose-synthetic-tune/dataset-coco/custom-configs/{self.config}'
+        self.work_dir = f'mmpose-synthetic-tune/models/_train-{current_time}-{notes}'
+        self.resume = '--resume' if resume == True else ''
+
+        self.args = [
+            self.command,
+            self.script,
+            self.config_path,
+            '--work-dir',
+            self.work_dir,
+            self.resume,
+        ]
+
+    def train(self):
+        subprocess.run(self.args)
+
+    def visualize_results(self, model_ckpt, vis_input):
+        poser_model = {
+            "pose2d": f'{self.work_dir}/{self.config}',
+            "pose2d_weights": f'{self.work_dir}/{model_ckpt}',
+        }
+
+        inferencer = MMPoseInferencer(**poser_model, **self.detector_model, device='cuda:0')
+
+        input_path = vis_input
+        output_path = f'{self.work_dir}/vis_results'
+
+        result_generator = inferencer(
+            input_path,
+            radius=3,
+            thickness=1,
+            vis_out_dir=output_path,
+            draw_heatmap=True,
+            det_cat_ids=5
+        )
+
+        results = [res for res in result_generator]
+
+
+#%%
+base_hrnet = MMPoseModelCoach(
+    config='cow-20kp-base-hrnet.py',
+    notes='base-hrnet'
+)
+base_hrnet.train()
+
+#%%
+base_apk10k = MMPoseModelCoach(
+    config='cow20kp-base-ap10k-hrnet.py',
+    notes='base-ap10k'
+)
+base_apk10k.train()
+#%%
+base_apk10k.visualize_results(
+    model_ckpt='epoch_210.pth',
+    vis_input='mmpose-synthetic-tune/dataset-coco/data/test-footage/cow.png'
+)
--- notebooks/mmpose_inference.py
+++ notebooks/mmpose_inference.py
@@ -1,44 +1,40 @@
 #%%
 from mmpose.apis import MMPoseInferencer
-import matplotlib.pyplot as plt
-
-import os
-os.getcwd()
-#%%
+# import matplotlib.pyplot as plt
 
 # Det models
 rtmdet = dict(
-    det_model       = 'mmdetection/configs/rtmdet/rtmdet_l_swin_b_p6_4xb16-100e_coco.py',
-    det_weights     = 'checkpoints/rtmdet_l_swin_b_p6_4xb16-100e_coco-a1486b6f.pth',
+    det_model       = '../../mmdetection/configs/rtmdet/rtmdet_l_swin_b_p6_4xb16-100e_coco.py',
+    det_weights     = '../../checkpoints/rtmdet_l_swin_b_p6_4xb16-100e_coco-a1486b6f.pth',
 )
 # Pose models
-ap10k_path = 'mmpose-synthetic-tune/models/pretrained-hrnet_w48_ap10k_256x256-d95ab412_20211029'
+ap10k_path = '../../mmpose-synthetic-tune/models/pretrained-hrnet_w48_ap10k_256x256-d95ab412_20211029'
 ap10k = dict(
     pose2d          = f'{ap10k_path}/td-hm_hrnet-w48_8xb64-210e_ap10k-256x256.py',
     pose2d_weights  = f'{ap10k_path}/hrnet_w48_ap10k_256x256-d95ab412_20211029.pth',
 )
 
-hrnet_path = 'mmpose-synthetic-tune/models/pretrained-hrnet_w48-8ef0771d'
+hrnet_path = '../../mmpose-synthetic-tune/models/pretrained-hrnet_w48-8ef0771d'
 hrnet = dict(
     pose2d          = f'{hrnet_path}/td-hm_hrnet-w48_8xb64-210e_ap10k-256x256.py',
     pose2d_weights  = f'{hrnet_path}/hrnet_w48-8ef0771d.pth'
 )
-cow20k_path = 'mmpose-synthetic-tune/models/train-2024-04-11_11-45-58'
+cow20k_path = '../../mmpose-synthetic-tune/models/_train-2024-04-15_10-17-13-base-hrnet'
 cow20k = dict(
     pose2d          = f'{cow20k_path}/td-hm_hrnet-w48_8xb64-210e_20kp-256x256.py',
     pose2d_weights  = f'{cow20k_path}/epoch_210.pth',
 )
 
 detector_model      = rtmdet
-poser_model         = ap10k
-model_path          = ap10k_path
+poser_model         = cow20k
+model_path          = cow20k_path
     
 inferencer = MMPoseInferencer(**poser_model, **detector_model, device='cuda:0')
 
 #%%
 # file = 'cow.png'
 # input_path = f'../../data/{file}'
-input_path = 'mmpose-synthetic-tune/dataset-coco/data/20kp/images/train'
+input_path = '../../mmpose-synthetic-tune/dataset-coco/data/20kp/images/train'
 output_path = f'{model_path}/vis_results'
 
 result_generator = inferencer(
